2f29f7de5456f38d31bb2aa4ce02f26dd473620b
sunrisebanana <sunrisebanana@gmail.com>
2020-02-20 10:54:42 -0500

    fixed matches per round issues, changed progression

--- backend/tests.py
+++ backend/tests.py
@@ -3,6 +3,7 @@ from tournament import Tournament, Bracket, BracketTypes, Round, Match
 from player import User
 
 class TestBracketCreation(unittest.TestCase):
+    
     def test_doubleElim(self):
         entrant1 = User("entrant1")
         entrant2 = User("entrant2")
@@ -14,10 +15,23 @@ class TestBracketCreation(unittest.TestCase):
         entrant8 = User("entrant8")
         tournament = Tournament([entrant1, entrant2, entrant3, entrant4, entrant5, entrant6, entrant7, entrant8], BracketTypes.DOUBLEELIMINATION)
 
-        self.assertTrue(tournament.bracket.rounds[0].matches[0].entrant1 == entrant1)
-        self.assertTrue(tournament.bracket.rounds[0].matches[0].loserPlaysInMatch == tournament.bracket.rounds[0].matches[1].loserPlaysInMatch)
-        self.assertTrue(tournament.bracket.rounds[0].matches[0].winnerPlaysInMatch == tournament.bracket.rounds[0].matches[1].winnerPlaysInMatch)
-        self.assertTrue(tournament.bracket.rounds[5].matches[0].winnerPlaysInMatch == tournament.bracket.rounds[1].matches[0].loserPlaysInMatch)
+        for match in tournament.bracket.rounds[0].matches:
+            match.inputScore(2, 1, match.entrant1 , match.entrant2)
+        for round in tournament.bracket.rounds:
+            for match in round.matches:
+                if match.entrantInMatch(tournament.bracket.rounds[2].matches[0].loser):
+                    print(str(round.roundNumber) + str(match.matchNumber))
+
+        #for i in range(0, len(tournament.bracket.rounds)):
+        #    tournament.bracket.rounds[i].handleProgression()
+        self.assertTrue(tournament.bracket.rounds[0].matches[0].entrantInMatch(entrant1))
+        self.assertTrue(tournament.bracket.rounds[0].matches[0].entrantInMatch(entrant8))
+        self.assertTrue(tournament.bracket.rounds[1].matches[0].entrantInMatch(tournament.bracket.rounds[0].matches[0].winner))
+        #self.assertTrue(tournament.bracket.rounds[0].matches[0].winnerPlaysInMatch == tournament.bracket.rounds[0].matches[1].winnerPlaysInMatch)
+        self.assertTrue(tournament.bracket.rounds[5].matches[0].entrantInMatch(tournament.bracket.rounds[0].matches[0].loser))
+        self.assertTrue(tournament.bracket.rounds[7].matches[0].entrantInMatch(tournament.bracket.rounds[6].matches[0].winner))
+        self.assertTrue(tournament.bracket.rounds[8].matches[0].entrantInMatch(tournament.bracket.rounds[2].matches[0].loser))
+        self.assertTrue(tournament.bracket.rounds[3].matches[0].entrantInMatch(tournament.bracket.rounds[8].matches[0].winner))
 
 if __name__ == '__main__':
     unittest.main()
--- backend/tournament.py
+++ backend/tournament.py
@@ -3,9 +3,9 @@ from player import User
 import math
 
 class BracketTypes(Enum):
-    DOUBLEELIMINATION = 1  
+    DOUBLEELIMINATION = 1
 
- 
+
 class Tournament:
     def __init__(self, bracket):
         self.bracket = bracket
@@ -21,22 +21,22 @@ class Bracket:
         self.ceilPlayers = int(2**(math.ceil(math.log(len(self.entrants)))))
         if (bracketType == BracketTypes.DOUBLEELIMINATION):
             self.makeDoubleElimBracket()
-            
+
     def makeDoubleElimBracket(self):
         # grand finals + reset round
-        self.numWinnersRounds = int(math.log(self.ceilPlayers,2)+2) 
+        self.numWinnersRounds = int(math.log(self.ceilPlayers,2)+2)
         self.numLosersRounds = int((math.log(self.ceilPlayers,2)) + math.log(math.log(self.ceilPlayers,2),2))
         self.numTotalRounds = self.numLosersRounds + self.numWinnersRounds
         self.rounds = []
         for i in range(1, self.numWinnersRounds+1):
             if i == self.numWinnersRounds - 1 or i == self.numWinnersRounds:
                 self.rounds.append(Round(i, self, 1, isWinners=True))
-            else: 
+            else:
                 self.rounds.append(Round(i, self, int(self.ceilPlayers/(2**i)), isWinners=True))
 
         for i in range(1, self.numLosersRounds+1):
             if i == 1:
-                self.rounds.append(Round(i, self, int(self.ceilPlayers/(2**(2*(math.ceil(i/2))))), isWinners=False))
+                self.rounds.append(Round(i, self, int(self.rounds[0].numMatches/2), isWinners=False))
             elif i%2 == 0:
                 self.rounds.append(Round(i, self, self.rounds[self.numLosersRounds + i-1].numMatches, isWinners=False))
             else:
@@ -56,14 +56,16 @@ class Round:
         self.matches = []
         self.isWinners = isWinners
         self.numMatches = numMatches
+        j = 0
         for i in range(0, self.numMatches):
+            j+=1
             if roundNumber == 1 and isWinners == True:
                 if (len(self.bracket.entrants) < self.bracket.ceilPlayers - i):
-                    self.matches.append(Match(self.bracket.entrants[i], None, self))
-                else: 
-                    self.matches.append(Match(self.bracket.entrants[i], self.bracket.entrants[self.bracket.ceilPlayers - i - 1], self))
-            else: 
-                self.matches.append(Match(None, None, self))
+                    self.matches.append(Match(self.bracket.entrants[i], None, self, j))
+                else:
+                    self.matches.append(Match(self.bracket.entrants[i], self.bracket.entrants[self.bracket.ceilPlayers - i - 1], self, j))
+            else:
+                self.matches.append(Match(None, None, self, j))
     def handleProgression(self):
         if self.roundNumber == self.bracket.numWinnersRounds and self.isWinners == True:
             return
@@ -72,49 +74,69 @@ class Round:
             return
         for i in range(0, len(self.matches)):
             if (self.isWinners == True):
-                self.matches[i].winnerPlaysInMatch(self.bracket.rounds[self.roundNumber].matches[int(math.floor(i/2))])
+                self.bracket.rounds[self.roundNumber].matches[int(math.floor(i/2))].setEntrant(self.matches[i].winner)
                 # TODO implement double jeopardy avoidance (https://blog.smash.gg/changes-in-the-world-of-brackets-695ecb777a4c)
                 if self.roundNumber == 1:
-                    self.matches[i].loserPlaysInMatch(self.bracket.rounds[self.bracket.numWinnersRounds].matches[int(math.floor(i/2))])
-                else: 
-                    # should work, but no dj avoidance 
-                    self.matches[i].loserPlaysInMatch(self.bracket.rounds[self.bracket.numWinnersRounds + (self.roundNumber-1)*2 - 1].matches[i])
+                    self.bracket.rounds[self.bracket.numWinnersRounds].matches[int(math.floor(i/2))].setEntrant(self.matches[i].loser) 
+                else:
+                    # should work, but no dj avoidance
+                    self.bracket.rounds[self.bracket.numWinnersRounds + (self.roundNumber-1)*2 - 1].matches[i].setEntrant(self.matches[i].loser)
                     #placeInLosers += 2
             if (self.isWinners == False):
-                ## place winner of losers finals in grand finals 
+                ## place winner of losers finals in grand finals
                 if self.roundNumber == self.bracket.numLosersRounds:
-                    self.matches[i].winnerPlaysInMatch(self.bracket.rounds[self.bracket.numWinnersRounds].matches[i])
+                    self.bracket.rounds[self.bracket.numWinnersRounds-2].matches[i].setEntrant(self.matches[i].winner)
                 ##if next round has the same number of matches as the current round
                 elif self.roundNumber%2 == 1:
-                    self.matches[i].winnerPlaysInMatch(self.bracket.rounds[self.bracket.numWinnersRounds + self.roundNumber].matches[i])
-                ##if next round plays opponents from the winners bracket aka if next round in losers bracket has less matches than this round 
+                    self.bracket.rounds[self.bracket.numWinnersRounds + self.roundNumber].matches[i].setEntrant(self.matches[i].winner)
+                ##if next round plays opponents from the winners bracket aka if next round in losers bracket has less matches than this round
                 else:
-                    self.matches[i].winnerPlaysInMatch(self.bracket.rounds[self.bracket.numWinnersRounds + self.roundNumber].matches[int(math.floor(i/2))])
+                    self.bracket.rounds[self.bracket.numWinnersRounds + self.roundNumber].matches[int(math.floor(i/2))].setEntrant(self.matches[i].winner)
 
 class Match:
-    def __init__(self, entrant1, entrant2, matchRound):
+    def __init__(self, entrant1, entrant2, matchRound, matchNumber):
         self.entrant1 = entrant1
         self.entrant2 = entrant2
         self.matchRound = matchRound
+        self.matchNumber = matchNumber
+        # Winner/loser/entrants put as lists because we need to copy the address of the list itself,
+        # so when the winner of the match gets set, the match in which the winner plays in has it's entrant 
+        # updated as well
+        self.winner = [User("")]
+        self.loser = [User("")]
 
-    def winnerPlaysInMatch(self, nextMatch):
-        self.winnerPlaysInMatch = nextMatch
+    #def winnerPlaysInMatch(self, nextMatch):
+    #    self.winnerPlaysInMatch = nextMatch
 
-    def loserPlaysInMatch(self, nextMatch):
-        self.loserPlaysInMatch = nextMatch
+    #def loserPlaysInMatch(self, nextMatch):
+    #    self.loserPlaysInMatch = nextMatch
 
     def inputScore(self, scoreEntrant1, scoreEntrant2, winner, loser):
         ##TODO
         self.scoreEntrant1 = scoreEntrant1
         self.scoreEntrant2 = scoreEntrant2
-        self.winner = winner
-        self.proceedToMatch(winner, self.winnerPlaysInMatch)
-        self.proceedToMatch(loser, self.loserPlaysInMatch)
+        self.winner[0] = winner
+        self.loser[0] = loser
 
-    def proceedToMatch(self, entrant, match):
-        if match.entrant1 == None:
-            match.entrant1 = entrant
-        elif match.entrant2 == None:
-            match.entrant2 = entrant
+    def entrantInMatch(self, entrant):
+        if self.entrant1 == entrant:
+            return True
+        elif self.entrant2 == entrant:
+            return True
         else:
-            print("The match " + entrant.profile.tag + " is trying to proceed to is full! Error")
+            return False
+
+    def setEntrant(self, entrant):
+        if self.entrant1 == None:
+            self.entrant1 = entrant
+        elif self.entrant2 == None: 
+            self.entrant2 = entrant
+        elif self.entrant1[0] == None:
+            self.entrant1[0] = entrant[0]
+        elif self.entrant2[0] == None:
+            self.entrant2[0] = entrant[0]
+        else:
+            if self.matchRound.isWinners:
+                print("The winners round/match " + str(self.matchRound.roundNumber) + "/" + str(self.matchNumber) + " that user " + entrant[0].username + " is trying to be added to is full! Error")
+            else:
+                print("The losers round/match " + str(self.matchRound.roundNumber) + "/" + str(self.matchNumber) + " that user " + entrant[0].username + " is trying to be added to is full! Error")
