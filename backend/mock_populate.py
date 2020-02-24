9c67ac99f4357a7545768de30c2762c8e1a9c732
alextrosta <alextrosta@gmail.com>
2020-02-24 06:50:30 -0500

    updated reqs for use with venv (lighter env)

--- backend/mock_populate.py
+++ backend/mock_populate.py
@@ -1,42 +1,295 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('autoreload', '2')
 from app import db
-from app.models import (
-    Bracket,
-    Match,
-    Round,
+import numpy as np
+
+from app.models import Tournament as TournamentModel
+from app.models import Bracket as BracketModel
+from app.models import Match as MatchModel
+from app.models import Round as RoundModel
+from app.models import User as UserModel
+
+from tournament import Tournament
+
+import numpy as np
+
+from sqlalchemy.sql.expression import func, select
+
+from tournament import (
+    BracketTypes,
     Tournament,
-    User, 
+    Match
 )
-from sqlalchemy.sql.expression import (
-    func, 
-    select
-)
+
+
+# In[2]:
+
+
+import uuid
+
+
+# In[3]:
+
+
+str(uuid.uuid4())
+
+
+# In[4]:
+
+
+db.session.close()
+
+
+# In[5]:
+
+
+def purge(Model):
+    [db.session.delete(row) for row in Model.query.all()]
+
+
+# In[6]:
+
+
+# purge all tables first
+
+for Model in [BracketModel, TournamentModel, MatchModel, RoundModel, UserModel]:
+    purge(Model)
+db.session.commit()
+
+
+# In[7]:
+
+
+# add all users to users table
+users = [
+    UserModel(username='TPN', email='TPN@example.com'.lower()), 
+    UserModel(username='AngryFalco', email='AngryFalco@example.com'.lower()), 
+    UserModel(username='Sunrisebanana', email='Sunrisebanana@example.com'.lower()), 
+    UserModel(username='Ptolemy', email='Ptolemy@example.com'.lower()), 
+    UserModel(username='Vik', email='Vik@example.com'.lower()), 
+    UserModel(username='Kevin', email='Kevin@example.com'.lower()), 
+    UserModel(username='Spaceghost', email='Spaceghost@example.com'.lower()), 
+    UserModel(username='Burnaby', email='Burnaby@example.com'.lower()), 
+]
+[db.session.add(u) for u in users]
+db.session.commit()
+
+
+# In[8]:
 
 
 # Fall Charity LAN 2018 Melee Singles users
-users = [
-    User(username='TPN', email='TPN@example.com'.lower()), 
-    User(username='AngryFalco', email='AngryFalco@example.com'.lower()), 
-    User(username='Sunrisebanana', email='Sunrisebanana@example.com'.lower()), 
-    User(username='Ptolemy', email='Ptolemy@example.com'.lower()), 
-    User(username='Vik', email='Vik@example.com'.lower()), 
-    User(username='Kevin', email='Kevin@example.com'.lower()), 
-    User(username='Spaceghost', email='Spaceghost@example.com'.lower()), 
-    User(username='Burnaby', email='Burnaby@example.com'.lower()), 
+N_COMPETITORS = 8
+seeds = [i+1 for i in range(N_COMPETITORS)]
+usernames = [
+    'TPN',
+    'AngryFalco',
+    'Sunrisebanana',
+    'Ptolemy',
+    'Vik',
+    'Kevin',
+    'Spaceghost',
+    'Burnaby',
 ]
+np.random.shuffle(usernames)
+np.random.shuffle(seeds)
 
-# set up tournament with all users
-t = \
-    Tournament(
-        n_entrants = len(users), 
-        tournament_name='Fall Charity LAN 2018 Melee Singles',
-        organizer_id
-    )
 
-User.query.all()
+# In[9]:
 
-# rounds = [
-#     Round(user_1=users[0])
-# ]
 
-[db.session.add(u) for u in users]
-db.session.commit()
+tuple_list = [(usernames[i], seeds[i]) for i in range(N_COMPETITORS)]
+
+
+# In[10]:
+
+
+btype = BracketTypes.DOUBLE_ELIMINATION
+t = Tournament(tuple_list, btype)
+
+
+# In[11]:
+
+
+tournament_name = 'Fall Charity LAN 2018 Melee Singles'
+TO = t.bracket.entrants[0]
+
+# initial post to db
+t.post_to_db(tournament_name, TO,)
+
+
+# In[12]:
+
+
+# post self references in matches separately
+for r in t.bracket.rounds:
+    for m in r.matches:
+        m.post_self_refs()
+
+
+# In[13]:
+
+
+import sqlite3
+import pandas as pd
+
+# Create the connection
+cnx = sqlite3.connect(r'./app.db')
+
+q = """
+    select * from user where
+    username like '%TPN%'
+"""
+q = """
+    select * from user
+"""
+# create the dataframe from a query
+df = pd.read_sql_query(q, cnx)
+df
+
+
+# In[14]:
+
+
+pd.read_sql_query('select * from tournament', cnx)
+
+
+# In[15]:
+
+
+pd.read_sql_query('select * from bracket', cnx)
+
+
+# In[16]:
+
+
+pd.read_sql_query('select * from bracket_entrants', cnx)
+
+
+# In[17]:
+
+
+pd.read_sql_query('select * from round', cnx)
+
+
+# In[18]:
+
+
+df = pd.read_sql_query('select * from match', cnx)
+df
+# df.loc[df['uuid'] == '94de92a2-13ac-4e9a-9bf2-78636100a7b1']
+
+
+# In[ ]:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
