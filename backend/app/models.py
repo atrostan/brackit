25d4fffff93eff8fc410f8ef0e459a51b153b0a6
alextrosta <alextrosta@gmail.com>
2020-03-07 17:50:19 -0500

    add role attr to UserModel to have Guests in Tourn

--- backend/app/models.py
+++ backend/app/models.py
@@ -22,9 +22,9 @@ bracket_entrants = db.Table('bracket_entrants',
 									primary_key=True)
 							)
 
-
 class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
+	role = db.Column(db.String(10))
 	username = db.Column(db.String(64), index=True, unique=True)
 	email = db.Column(db.String(120), index=True, unique=True)
 	password_hash = db.Column(db.String(128))
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -49,64 +49,11 @@ from datetime import datetime
 from flask_httpauth import HTTPBasicAuth
 auth = HTTPBasicAuth()
 
-# @app.route('/edit_profile', methods=['GET', 'POST'])
-# @login_required
-# def edit_profile():
-#     form = EditProfileForm()
-#     if form.validate_on_submit():
-#         current_user.username = form.username.data
-#         current_user.about_me = form.about_me.data
-#         db.session.commit()
-#         flash('Your changes have been saved.')
-#         return redirect(url_for('edit_profile'))
-#     elif request.method == 'GET':
-#         form.username.data = current_user.username
-#         form.about_me.data = current_user.about_me
-#     return render_template('edit_profile.html', title='Edit Profile',
-#                             form=form)
-
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
-
-@app.route('/')
-@app.route('/index')
-@login_required
-def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template("index.html", title='Home Page', posts=posts)
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('index'))
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(username=form.username.data).first()
-#         if user is None or not user.check_password(form.password.data):
-#             flash('Invalid username or password')
-#             return redirect(url_for('login'))
-#         login_user(user, remember=form.remember_me.data)
-#         next_page = request.args.get('next')
-#         if not next_page or url_parse(next_page).netloc != '':
-#             next_page = url_for('index')
-#         return redirect(next_page)
-#     # return jsonify(form.)
-#     return render_template('login.html', title='Sign In', form=form)
-
-
 
 @app.route('/logout')
 def logout():
@@ -137,7 +84,7 @@ def new_user():
         abort(400) # missing arguments
     if UserModel.query.filter_by(username = username).first() is not None:
         abort(400) # existing user
-    user = UserModel(username = username, email=email)
+    user = UserModel(username = username, email=email, role='User')
     user.set_password(password)
     db.session.add(user)
     db.session.commit()
